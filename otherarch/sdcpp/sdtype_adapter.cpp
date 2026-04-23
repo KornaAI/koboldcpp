2cde0bffd2fa8755c942897153bc414abbc00b24
Concedo <39025047+LostRuins@users.noreply.github.com>
2026-04-23 20:06:04 +0800

    minor text edit

--- otherarch/sdcpp/sdtype_adapter.cpp
+++ otherarch/sdcpp/sdtype_adapter.cpp
@@ -506,8 +506,7 @@ bool sdtype_load_model(const sd_load_model_inputs inputs) {
     sd_ctx = new_sd_ctx(&params);
 
     if (sd_ctx == NULL) {
-        printf("\nError: KCPP SD Failed to create context!\nIf using Flux/SD3.5, make sure you have ALL files required (e.g. VAE, T5, Clip...) or baked in!\n");
-        printf("Otherwise, if you are using GGUF format, you can try the original .safetensors instead (Comfy GGUF not supported)\n");
+        printf("\nError: Image generation failed to setup!\nMake sure you have ALL files required (e.g. VAE, T5, Clip...) or baked into the model!\n");
         return false;
     }
 
